efbf655f6c72ea150b19e688f784650c62f54bed
yueranl <yueranl03@gmail.com>
2025-01-09 12:24:14 +0100

    Make osm parser work

--- mordern_cpp/reactor/utility/openstreetmap/src/parse_osm.cc
+++ mordern_cpp/reactor/utility/openstreetmap/src/parse_osm.cc
@@ -1,23 +1,35 @@
-#include <osmium/io/reader.hpp>
-#include <osmium/handler.hpp>
-#include <osmium/visitor.hpp>
 #include <iostream>
 
-class OSMHandler : public osmium::handler::Handler {
+#include <osmium/handler.hpp>
+#include <osmium/osm/node.hpp>
+#include <osmium/osm/way.hpp>
+#include <osmium/io/any_input.hpp>
+#include <osmium/visitor.hpp>
+#include <osmium/index/map/sparse_mem_array.hpp>
+#include <osmium/handler/node_locations_for_ways.hpp>
+
+class MyHandler : public osmium::handler::Handler {
 public:
-    void node(const osmium::Node& node) {
-        std::cout << "Node ID: " << node.id() 
-                  << " Location: " << node.location() << std::endl;
-    }
     void way(const osmium::Way& way) {
-        std::cout << "Way ID: " << way.id() << std::endl;
+        std::cout << "way " << way.id() << '\n';
+        for (const auto& n : way.nodes()) {
+            std::cout << n.ref() << ": " << n.lon() << ", " << n.lat() << '\n';
+        }
     }
 };
 
 int main() {
-    osmium::io::Reader reader("berlin.osm.pbf");
-    OSMHandler handler;
-    osmium::apply(reader, handler);
+    auto otypes = osmium::osm_entity_bits::node | osmium::osm_entity_bits::way;
+    osmium::io::Reader reader{"/home/muzi/Documents/exer_repo_cpp/build/berlin.osm.pbf", otypes};
+
+    namespace map = osmium::index::map;
+    using index_type = map::SparseMemArray<osmium::unsigned_object_id_type, osmium::Location>;
+    using location_handler_type = osmium::handler::NodeLocationsForWays<index_type>;
+
+    index_type index;
+    location_handler_type location_handler{index};
+
+    MyHandler handler;
+    osmium::apply(reader, location_handler, handler);
     reader.close();
-    return 0;
 }
