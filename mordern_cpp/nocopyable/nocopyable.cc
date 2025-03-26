ceafff8f3a7663a15e58101e76ad415a9b22f032
yueranl <yueranl03@gmail.com>
2025-03-26 10:58:07 +0100

    Daily exercise for smart pointer

--- mordern_cpp/nocopyable/nocopyable.cc
+++ mordern_cpp/nocopyable/nocopyable.cc
@@ -3,20 +3,45 @@
 #include <memory>
 #include <string>
 #include <map>
+#include <functional>
 
 class Object
 {
 public:
-    Object() {std::cout << "Object constructed!" << std::endl;}
+    Object(const std::string& key): key_(key) {std::cout << "Object constructed!" << std::endl;}
     ~Object() {std::cout << "Object destructed!" << std::endl;}
+    std::string key()
+    {
+        return key_;
+    }
+private:
+    std::string key_;
 };
 
 template<typename T>
-class ObjectFactory: public boost::noncopyable
+class ObjectFactory: public boost::noncopyable, public std::enable_shared_from_this<ObjectFactory<T>>
 {
 public:
     std::shared_ptr<T> get(const std::string& key);
 private:
+    static void WeakDelCallback(std::weak_ptr<ObjectFactory<T>> wPtr, T *object)
+    {
+        std::shared_ptr<ObjectFactory<T>> sPtr = wPtr.lock();
+        if(sPtr)
+        {
+            sPtr->deleteObject(object);
+        }
+        delete object;
+    }
+
+    void deleteObject(T *object)
+    {
+        if(object)
+        {
+            factory_.erase(object->key());
+        }
+    }
+
     std::map<std::string, std::weak_ptr<T>> factory_;
 };
 
@@ -27,7 +52,9 @@ std::shared_ptr<T> ObjectFactory<T>::get(const std::string& key) {
     pObj = wObj.lock();
     if(!pObj) {
         std::cout << "Create new shared pointer!" << std::endl;
-        pObj = std::make_shared<T>();
+        pObj.reset(new T(key), std::bind(&ObjectFactory<T>::WeakDelCallback, 
+            std::weak_ptr<ObjectFactory<T>>(std::enable_shared_from_this<ObjectFactory<T>>::shared_from_this()), 
+            std::placeholders::_1)); // to avoid that Factory is deconstructed before Object
         wObj = pObj;
     }
     return pObj;
@@ -35,8 +62,8 @@ std::shared_ptr<T> ObjectFactory<T>::get(const std::string& key) {
 
 int main()
 {
-    ObjectFactory<Object> factory;
-    std::shared_ptr<Object> pObj = factory.get("Lego");
+    std::shared_ptr<ObjectFactory<Object>> factory = std::make_shared<ObjectFactory<Object>>();
+    std::shared_ptr<Object> pObj = factory->get("Lego");
     std::cout << pObj.use_count() << std::endl;
     return 0;
 }
