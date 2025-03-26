7a46aa94b098a9f348639972110bb0ba60138042
yueranl <yueranl03@gmail.com>
2025-03-26 12:16:37 +0100

    Update code for noncopyable exercise

--- mordern_cpp/nocopyable/nocopyable.cc
+++ mordern_cpp/nocopyable/nocopyable.cc
@@ -1,15 +1,14 @@
 #include <iostream>
-#include <boost/noncopyable.hpp>
 #include <memory>
-#include <string>
-#include <map>
 #include <functional>
+#include <map>
+#include <boost/noncopyable.hpp>
 
 class Object
 {
 public:
     Object(const std::string& key): key_(key) {std::cout << "Object constructed!" << std::endl;}
-    ~Object() {std::cout << "Object destructed!" << std::endl;}
+    ~Object() {std::cout << "Object destrcuted!" << std::endl;}
     std::string key()
     {
         return key_;
@@ -22,48 +21,51 @@ template<typename T>
 class ObjectFactory: public boost::noncopyable, public std::enable_shared_from_this<ObjectFactory<T>>
 {
 public:
-    std::shared_ptr<T> get(const std::string& key);
+    std::shared_ptr<T> get(const std::string& key)
+    {
+        std::shared_ptr<T> sPtr;
+        std::weak_ptr<T>& wPtr = factory_[key];
+        sPtr = wPtr.lock();
+        if(!sPtr)
+        {
+            sPtr.reset(
+                new Object(key), 
+                std::bind(
+                    &ObjectFactory::WeakDeleteCallback, 
+                    std::weak_ptr<ObjectFactory<T>>(std::enable_shared_from_this<ObjectFactory<T>>::shared_from_this()), 
+                    std::placeholders::_1
+                )
+            );
+            wPtr = sPtr;
+        }
+        return sPtr;
+    }
 private:
-    static void WeakDelCallback(std::weak_ptr<ObjectFactory<T>> wPtr, T *object)
+    static void WeakDeleteCallback(std::weak_ptr<ObjectFactory<T>> wPtr, T *obj)
     {
         std::shared_ptr<ObjectFactory<T>> sPtr = wPtr.lock();
         if(sPtr)
         {
-            sPtr->deleteObject(object);
+            sPtr->deleteObj(obj);
         }
-        delete object;
+        delete obj;
     }
 
-    void deleteObject(T *object)
+    void deleteObj(T *obj)
     {
-        if(object)
+        if(obj)
         {
-            factory_.erase(object->key());
+            factory_.erase(obj->key());
         }
     }
 
-    std::map<std::string, std::weak_ptr<T>> factory_;
+    std::map<std::string, std::weak_ptr<Object>> factory_;
 };
-
-template<typename T>
-std::shared_ptr<T> ObjectFactory<T>::get(const std::string& key) {
-    std::shared_ptr<T> pObj;
-    std::weak_ptr<T>& wObj = factory_[key];
-    pObj = wObj.lock();
-    if(!pObj) {
-        std::cout << "Create new shared pointer!" << std::endl;
-        pObj.reset(new T(key), std::bind(&ObjectFactory<T>::WeakDelCallback, 
-            std::weak_ptr<ObjectFactory<T>>(std::enable_shared_from_this<ObjectFactory<T>>::shared_from_this()), 
-            std::placeholders::_1)); // to avoid that Factory is deconstructed before Object
-        wObj = pObj;
-    }
-    return pObj;
-}
 
 int main()
 {
     std::shared_ptr<ObjectFactory<Object>> factory = std::make_shared<ObjectFactory<Object>>();
-    std::shared_ptr<Object> pObj = factory->get("Lego");
-    std::cout << pObj.use_count() << std::endl;
+    std::shared_ptr<Object> obj = factory->get("IBN: 47877");
+    std::cout << obj.use_count() << std::endl;
     return 0;
 }
